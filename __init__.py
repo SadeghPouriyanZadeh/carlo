ba1b847b488552e65dc31bd097debd26e8c36736
SadeghPouriyanZadeh <19951995aA@>
2021-07-25 16:27:30 +0430

    postprocess added to package namespace

--- __init__.py
+++ __init__.py
@@ -3,3 +3,4 @@ from carlo.nanofiber import NanoFiber
 from carlo.environment import Environment
 import carlo.materials
 from carlo.utils import save_simulation, load_simulation
+from carlo.postprocessing import PostProcess
