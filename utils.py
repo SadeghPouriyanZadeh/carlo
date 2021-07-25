fb476a8b5ada423274f508f555f9e3ec1d552b10
SadeghPouriyanZadeh <19951995aA@>
2021-07-25 15:59:17 +0430

    add get_current_function

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pickle
 from pathlib import Path
 
+
 def get_molecular_diameter(solid_state_density, molweight):
     NA = 6.0221409e23  # avogadro's number
     PI = np.pi
@@ -17,18 +18,29 @@ def get_molecular_diameter(solid_state_density, molweight):
 def get_simulation_name():
     now = datetime.now()
     name_parts = now.year, now.month, now.day, now.hour, now.minute
-    name =  '_'.join([str(p) for p in name_parts]) + '.carlo'
+    name = "_".join([str(p) for p in name_parts]) + ".carlo"
     return name
 
 
 def save_simulation(simulation_obj, file_path=None):
     if not file_path:
         file_path = Path(get_simulation_name())
-    with open(file_path, 'wb') as file:
+    with open(file_path, "wb") as file:
         pickle.dump(simulation_obj, file)
     return file_path
 
 
 def load_simulation(file_path):
-    with open(file_path, 'rb') as file:
+    with open(file_path, "rb") as file:
         return pickle.load(file)
+
+
+def get_current_function(time_current_array, maximum_current):
+    index = -1
+    t, i = time_current_array[:, index]
+    b = (-1 / t) * np.log((maximum_current - i) / maximum_current)
+
+    def get_current(time):
+        return maximum_current * (1 - np.exp(-b * time))
+
+    return get_current
