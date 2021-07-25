d977310bede822275b060434d6482c600b54301a
SadeghPouriyanZadeh <19951995aA@>
2021-07-25 16:28:32 +0430

    add current estimation class

--- postprocessing.py
+++ postprocessing.py
@@ -1,9 +1,30 @@
 import numpy as np
 from .simulation import GasSensorSimulation
+from .utils import get_current_time_function
+import matplotlib.pyplot as plt
+
+
+class CurrentEstimator:
+    def __init__(self, postprocess_obj):
+        self.postprocess_obj = postprocess_obj
+        self.a = self.postprocess_obj.simulation_obj.maximum_current
+        self.b = self.calculate_b()
+
+    def calculate_b(self):
+        time_current_array = self.postprocess_obj.get_time_current_array()
+        index = -1
+        t, i = time_current_array[:, index]
+        b = (-1 / t) * np.log((self.a - i) / self.a)
+        return b
+
+    def __call__(self, time):
+        return self.a * (1 - np.exp(-self.b * time))
+
 
 class PostProcess:
     def __init__(self, simulation_obj: GasSensorSimulation):
         self.simulation_obj = simulation_obj
+        self.current_estimator = CurrentEstimator(self)
 
     def get_state_generator(self):
         flat_time_mtx = np.unique(self.simulation_obj.time_mtx.flatten())
@@ -30,3 +51,23 @@ class PostProcess:
             current = self.get_current_from_state(step_state)
             current_list.append(current)
         return np.array([np.array(time_list), np.array(current_list)])
+
+    def get_current_figure(self, datapoints=10):
+        time_current_array = self.get_time_current_array()
+        points = 20
+        interval = int(
+            (self.simulation_obj.nanofiber.n_x * self.simulation_obj.nanofiber.n_y)
+            / points
+        )
+        t = time_current_array[0, ::interval]
+        y_hat = self.current_estimator(t)
+        y = time_current_array[1, ::interval]
+
+        plt.figure(figsize=(8, 5), dpi=100)
+        plt.scatter(t, y * 1e9, color="red", label="Simulation Data")
+        plt.plot(t, y_hat * 1e9, color="blue", label="Fitted Curve")
+        plt.xlabel("t $[s]$")
+        plt.ylabel("I $[nA]$")
+        plt.legend(frameon=False)
+        fig = plt.gcf()
+        return fig
